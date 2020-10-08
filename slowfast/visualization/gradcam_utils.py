8ea5679722dac113680c191b13d4b2f49b358867
Universe <bigtree123433@gmail.com>
2020-10-08 19:37:45 +0900

    back

--- slowfast/visualization/gradcam_utils.py
+++ slowfast/visualization/gradcam_utils.py
@@ -160,7 +160,6 @@ class GradCAM:
             localization_map = localization_map.squeeze(dim=1)
             if localization_map.device != torch.device("cpu"):
                 localization_map = localization_map.cpu()
-            print("localization_map: ", localization_map)
             heatmap = self.colormap(localization_map)
             heatmap = heatmap[:, :, :, :, :3]
             # Permute input from (B, C, T, H, W) to (B, T, H, W, C)
